ca31549c175be411a53e6e09fbfa0c56dd865aeb
Felix Nogueira <124099611+felixnogueira@users.noreply.github.com>
2024-04-23 20:20:23 -0300

    operadores finalizado

--- Operadores/src/Operadores.java
+++ Operadores/src/Operadores.java
@@ -21,5 +21,50 @@ public class Operadores {
         concatenacao = "1"+(1+1+1);
 
         System.out.println(concatenacao);
+
+        //operador ternário
+
+        int a, b;
+        a = 6;
+        b = 6;
+
+        int resultado = a == b ? 1 : 0;
+
+        System.out.println(resultado);
+
+        //operador relacional
+
+        String nomeUm = "Fulano";
+        String nomeDois = "Fulano";
+        System.out.println(nomeUm == nomeDois);
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        if (numero1 == numero2) {
+            System.out.println("a nossa condição é verdadeira");
+        }
+        else {
+            System.out.println("a nossa condição é falsa");
+        }
+
+        boolean simNao = numero1 < numero2;
+
+        System.out.println("numeroUm é maior que numeroDois? " + simNao);
+
+        //operadores lógicos
+
+        boolean condicao1 = true;
+        boolean condicao2 = true;
+
+        if (condicao1 && condicao2) {
+            System.out.println("as duas condições são verdadeiras");
+        }
+
+        if (condicao1 || condicao2) {
+            System.out.println("pelo menos uma das condições é verdadeira");
+        }
+
+
     }
 }
---
+++ Operadores/src/OperadoresUnarios.java
@@ -0,0 +1,26 @@
+public class OperadoresUnarios {
+    public static void main(String[] args) {
+        int numero = 5;
+
+        numero = - numero;
+
+        System.out.println(- numero);
+
+        System.out.println(numero);
+
+        int numeroNovo = 5;
+
+        System.out.println(--numeroNovo);
+
+        System.out.println(++numeroNovo);
+
+        boolean variavel = true;
+        System.out.println(!variavel);
+
+        variavel = !variavel;
+
+        System.out.println(variavel);
+
+    }
+}
+
